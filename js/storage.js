8bfd74a7d1419e57a023a4b3b78c35066731f30b
Vieyra <vieyraurenabravo@gmail.com>
2021-06-29 19:16:05 -0600

    fixed/localStorage

--- js/storage.js
+++ js/storage.js
@@ -4,8 +4,7 @@ function saveLocal() {
   const saveTasks = JSON.parse(localStorage.getItem('tasks'));
   if (saveTasks) {
     for (let i = 0; i < saveTasks.length; i++) {
-      console.log(saveTasks[i].complete)
-      const local = new AddTask(saveTasks[i].id, saveTasks[i].name, saveTasks[i].select, saveTasks[i].time);
+      const local = new AddTask(saveTasks[i].id, saveTasks[i].name, saveTasks[i].time, saveTasks[i].value);
       local.addTaskDOM();
     }
   }
